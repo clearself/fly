b39c9013fbcecec14ea07a67ab0694c5d3e648cb
clearself <fuleiyang@qq.com>
2017-03-09 14:06:36 +0800

    add loading

--- works/wxsince/js/index.js
+++ works/wxsince/js/index.js
@@ -15,30 +15,26 @@ $(function(){
 			var n = t.currentStyle ? t.currentStyle[e] : document.defaultView.getComputedStyle(t, !1)[e];
 			return "opacity" == e ? Math.round(100 * parseFloat(n)) : parseInt(n)
 		}
-			var l=0;
-			var imgs;
-			var sum=0;
-			function chk(){
-				$('#load_text').text(parseInt(((sum-l)*100/sum))+"%");
-				console.log(l,sum);
-				l--;
-				if (l==0){
-					//加载完成
-					$('#load_text').text(((sum-l)*100/sum)+"%");
-					$('.load_box').css('opacity',0);
-					setTimeout(function(){
-						$('.load_box').css('display','none');
-					},1000)
-				}
-			}
-			
-			imgs=$("img");
-			sum=l=imgs.length;
-			for (var i=0;i<l;i++){
-				imgs[i].onload=chk;
-				imgs[i].onerror=chk;//无论图片是否加载成功，都执行指定方法
-			}
-        $('.wp-inner').fullpage({
+	
+	 //loading列表
+    var basePath = "image/"
+    var loader = new window.PxLoader();
+    // 要加载图片的列表
+    var fileList = ['bg1.jpg','qq.png','../audio/happynewyear.mp3'];
+    for (var i = 0; i < fileList.length; i++) {
+        loader.addImage(basePath + fileList[i]);
+    }
+    loader.addProgressListener(function (e) {
+        var percent = Math.round((e.completedCount / e.totalCount) * 100);
+        document.getElementById("load_text").innerHTML = ( "已加载 " + percent + " %");
+    });
+    loader.addCompletionListener(function () {
+        $("#loading_bg").remove();
+       $('.load_box').css('opacity',0);
+		setTimeout(function(){
+			$('.load_box').css('display','none');
+		},1000)
+		 $('.wp-inner').fullpage({
             change: function (e) {
                 // 移除动画属性
               	$('.page').eq(e.cur).find('.js-animate').each(function() {
@@ -65,6 +61,10 @@ $(function(){
                     });
             }
         });
+		
+    });
+    loader.start();
+	
         
         //适配所有手机重新设置尺寸
   //       var scaleW=window.innerWidth/320;
